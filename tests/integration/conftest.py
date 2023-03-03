ffa96504e12900de1efa3dd8625da1b625b5acee
Thomas Rausch <thomas@thrau.at>
2023-03-03 02:47:40 +0100

    play around with pytest plugin

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -21,7 +21,6 @@ from tests.integration.apigateway_fixtures import delete_rest_api, import_rest_a
 from tests.integration.test_es import install_async as es_install_async
 from tests.integration.test_opensearch import install_async as opensearch_install_async
 from tests.integration.test_terraform import TestTerraform
-from tests.store_police import StorePolice, StorePoliceResult
 
 logger = logging.getLogger(__name__)
 
@@ -33,8 +32,14 @@ startup_monitor_event = mp.Event()  # event that can be triggered to start local
 # collection of functions that should be executed to initialize tests
 test_init_functions = set()
 
+pytest_plugins = []
 if config.is_collect_metrics_mode():
-    pytest_plugins = "localstack.testing.pytest.metric_collection"
+    pytest_plugins.append("localstack.testing.pytest.metric_collection")
+
+TEST_CHECK_PERSISTENCE = True
+
+if TEST_CHECK_PERSISTENCE:
+    pytest_plugins.append("localstack.testing.pytest.persistence")
 
 
 @pytest.hookimpl()
@@ -193,18 +198,3 @@ def import_apigw(apigateway_client):
 
     for rest_api_id in rest_api_ids:
         delete_rest_api(apigateway_client, restApiId=rest_api_id)
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_teardown(item, *args):
-    # simple heuristic to get the service under test
-    module_name: str = item.module.__name__
-    service_name = module_name.split("_")[-1]
-
-    store_police = StorePolice(service_name)
-    control_result: StorePoliceResult = store_police.control()
-    if not control_result.ok:
-        logger.error(control_result.why)
-    assert control_result.ok
-
-    yield
